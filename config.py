49a5b38c39225c4e7e2c40689fa7a85dc1c93e5f
djroth25 <137537044+djroth25@users.noreply.github.com>
2023-07-30 16:46:08 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,23 +3,23 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/train"
-VAL_DIR = "data/val"
+TRAIN_DIR = "/content/Wound-Segmentation/TrainingConcatImages"
+VAL_DIR = "/content/Wound-Segmentation/ValidationConcatImages"
 LEARNING_RATE = 2e-4
 BATCH_SIZE = 16
 NUM_WORKERS = 2
-IMAGE_SIZE = 256
+IMAGE_SIZE = 512
 CHANNELS_IMG = 3
 L1_LAMBDA = 100
 LAMBDA_GP = 10
-NUM_EPOCHS = 500
+NUM_EPOCHS = 25
 LOAD_MODEL = False
 SAVE_MODEL = False
 CHECKPOINT_DISC = "disc.pth.tar"
 CHECKPOINT_GEN = "gen.pth.tar"
 
 both_transform = A.Compose(
-    [A.Resize(width=256, height=256),], additional_targets={"image0": "image"},
+    [A.Resize(width=512, height=512),], additional_targets={"image0": "image"},
 )
 
 transform_only_input = A.Compose(
